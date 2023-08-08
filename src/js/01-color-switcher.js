63c34ae85f3d9434fd7e6d5ab017583461105ed7
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-08-08 15:11:49 +0300

    Update 01-color-switcher.js

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -5,21 +5,24 @@ const body = document.querySelector('body');
 btnStart.addEventListener('click', onBtnStartClick);
 btnStop.addEventListener('click', onBtnStopClick);
 
-let disabledStopBtn = (btnStop.disabled = true);
-let disabledStartBtn = (btnStart.disabled = false);
+onPageStart();
+
+function onPageStart() {
+  btnStop.disabled = true;
+}
 
 function onBtnStartClick() {
   body.style.backgroundColor = getRandomHexColor();
   timerId = setInterval(() => {
     body.style.backgroundColor = getRandomHexColor();
   }, 1000);
-  disabledStartBtn = btnStart.disabled = true;
-  disabledStopBtn = btnStop.disabled = false;
+  btnStart.disabled = true;
+  btnStop.disabled = false;
 }
 
 function onBtnStopClick() {
-  disabledStartBtn = btnStart.disabled = false;
-  disabledStopBtn = btnStop.disabled = true;
+  btnStart.disabled = false;
+  btnStop.disabled = true;
   clearInterval(timerId);
 }
 
