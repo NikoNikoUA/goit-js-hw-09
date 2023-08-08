f7799e8cbb3f71e27123de645fb48cc69911307a
NikoNikoUA <128256922+NikoNikoUA@users.noreply.github.com>
2023-08-08 12:12:12 +0000

    Deploying to gh-pages from @ NikoNikoUA/goit-js-hw-09@63c34ae85f3d9434fd7e6d5ab017583461105ed7 🚀

---
+++ 01-color-switcher.33063eba.js
@@ -0,0 +1,2 @@
+const e=document.querySelector("[data-start]"),t=document.querySelector("[data-stop]"),d=document.querySelector("body");function r(){return`#${Math.floor(16777215*Math.random()).toString(16).padStart(6,0)}`}e.addEventListener("click",(function(){d.style.backgroundColor=r(),timerId=setInterval((()=>{d.style.backgroundColor=r()}),1e3),e.disabled=!0,t.disabled=!1})),t.addEventListener("click",(function(){e.disabled=!1,t.disabled=!0,clearInterval(timerId)})),t.disabled=!0;
+//# sourceMappingURL=01-color-switcher.33063eba.js.map
